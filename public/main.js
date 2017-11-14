6ecbb1295780bf14d0f1fc1dc8d01fe775413bb1
Alumno23 J901 <a23j901@campustls.local>
2017-11-14 09:49:38 -0500

    Agregar generos e intereses

--- public/main.js
+++ public/main.js
@@ -2,12 +2,22 @@ $(document).ready(function(){
   $('input[type="submit"]').on("click", function(event) {
   event.preventDefault();
   var first_name = $('#first_name').val();
+  var full_name = $('#full_name').val();
 /*  $.ajax('find.html',{
     method: 'post',
     data: {
       'first_name' : first_name
     }
   });*/
-  $.get('find.html',{'first_name': first_name});
+  $.post('find.html',{'first_name': first_name});
+  $.post('find.html',{'full_name': full_name});
+
+  var gender = $('input[name="gender"]:checked').val();
+  $.post('find.html', {'gender' : gender});
+
+  var interest = $('input[name="interest"]:checked').val();
+  $.post('find.html', {'interest' : interest});  
+
+
 });
 });
